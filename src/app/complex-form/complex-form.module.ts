067085e942535756409a11987983f44a1735a4a0
aqet <tientcheuigorcarel@gmail.com>
2024-12-10 13:18:06 +0100

    le formulaire est bon

--- src/app/complex-form/complex-form.module.ts
+++ src/app/complex-form/complex-form.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { ComplexFormRoutingModule } from './complex-form-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ComplexFormService } from './services/complex-form.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,7 +12,11 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     ComplexFormRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
+  ],
+  providers:[
+    ComplexFormService
   ]
 })
 export class ComplexFormModule { }
