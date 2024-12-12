6a502bd4d16aab0bfe9012cc192cfdd6622d98ef
ULRICH WABA <wabaulrich1234@gmail.com>
2024-12-12 19:17:32 +0100

    fin du troisieme module

---
+++ src/app/reactive-state/reactive-state.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveStateRoutingModule } from './reactive-state-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveStateRoutingModule
+  ]
+})
+export class ReactiveStateModule { }
